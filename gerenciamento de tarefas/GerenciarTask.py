524c2594eb9cc2ead6784a842a5ffb6d79249275
Rafael Souza <67163593+SDRafael@users.noreply.github.com>
2024-08-16 14:43:33 -0300

    Create GerenciarTask.py

---
+++ gerenciamento de tarefas/GerenciarTask.py
@@ -0,0 +1,12 @@
+class GerenciarTask:
+    def __init__(self) -> None:
+        
+        
+    def computarTask(task):
+        taskList = []
+        taskList.append(task)
+
+
+
+    def concluirTask(self):
+        self.situation = True        
